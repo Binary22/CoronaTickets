1cae309d91aaf9b2e98ed3d03711920c8e8df123
Santiago <santiacquarone@gmail.com>
2020-09-08 16:19:32 -0300

    Añadir exception de ejemplo

---
+++ src/excepciones/EjemploException.java
@@ -0,0 +1,16 @@
+package excepciones;
+
+/**
+ * Excepción de ejemplo para copiar cuando quieran hacer
+ * otras excepciones
+ * 
+ * @author elacquaxd
+ *
+ */
+@SuppressWarnings("serial")
+public class EjemploException extends Exception {
+
+    public EjemploException(String string) {
+        super(string);
+    }
+}
