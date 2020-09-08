7fbcb1b4236b802bc55e78a2ff7301868a6dfaed
Santiago <santiacquarone@gmail.com>
2020-09-08 16:09:00 -0300

    Añadir archivos de ejemplo de capa presentacion

---
+++ src/presentacion/AltaUsuario.java
@@ -0,0 +1,33 @@
+package presentacion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class AltaUsuario extends JInternalFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AltaUsuario frame = new AltaUsuario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AltaUsuario() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
