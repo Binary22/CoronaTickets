3b9234a1d35e25263729cf653ba2a67449428159
santicosta <santicosta710@gmail.com>
2020-09-10 15:11:34 -0300

    corregir interfaz iusuario

--- src/logica/IUsuario.java
+++ src/logica/IUsuario.java
@@ -6,21 +6,21 @@ import java.util.ArrayList;
 
 public interface IUsuario {
 	
-	public abstract void altaUsuario(String nickname,String nombre,String apellido,String mail, Date fechanac);
+	public void altaUsuario(String nickname,String nombre,String apellido,String mail, Date fechanac);
 	
-	public abstract void altaArtista(String desc,String bio, String web);
+	public void altaArtista(String desc,String bio, String web);
 	
-	public abstract void confirmarAltaUsuario();
+	public void confirmarAltaUsuario();
 	
-	public abstract ArrayList<String> mostrarUsuarios();
+	public ArrayList<String> mostrarUsuarios();
 	
-	public abstract DtUsuario mostrarDatos(String nickname);
+	public DtUsuario mostrarDatos(String nickname);
 	
-	public abstract void updateUsuario(String nombre,String apellido,Date fechaNac);
+	public void updateUsuario(String nombre,String apellido,Date fechaNac);
 	
-	public abstract void updateArtista(String nombre,String apellido,Date fechaNac,String desc,String bio,String web);
+	public void updateArtista(String nombre,String apellido,Date fechaNac,String desc,String bio,String web);
 
-	public abstract void confirmarUpdateUsuario();
+	public void confirmarUpdateUsuario();
 
 }
 
