aaaae37df789d093c7f2814e1d111c56a6697089
Santiago <santiacquarone@gmail.com>
2020-09-12 18:48:30 -0300

    Que descripcion se vea bien y es artista chico

--- src/presentacion/AltaUsuario.java
+++ src/presentacion/AltaUsuario.java
@@ -74,9 +74,9 @@ public class AltaUsuario extends JInternalFrame {
 		setClosable(true);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 162, 186, 28, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 35, 35, 32, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 35, 32, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE, 0.0, 0.0, 0.0};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 4.9E-324, 0.0, 0.0, 0.0};
 		getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblNickname = new JLabel("Nickname*");
