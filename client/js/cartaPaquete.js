ff8726097c266445316be6c314a916a62b8691cf
Joaquin <jotaseis@gmail.com>
2020-10-03 16:04:21 -0300

    detalles paquete.

--- client/js/cartaPaquete.js
+++ client/js/cartaPaquete.js
@@ -7,7 +7,7 @@ templatePaquete.innerHTML = `
         <p class="card-text">
         </p>
         <ul class="list-group list-group-horizontal">
-            <a href="#" class="list-group-item card-link active flex-fill">Ver Paquete</a>
+            <a href="../html/detallesPaquete.html" class="list-group-item card-link active flex-fill">Ver Paquete</a>
             <li id="precio" class="list-group-item flex-fill"></li>
         </ul>
     </div>
