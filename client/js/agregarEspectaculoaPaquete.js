0e7095fcaeb07c6f336cc13fe110e45fc3a888d7
Agustin Jose Amegeiras Quintillan <agustin.amegeiras@fing.edu.uy>
2020-10-04 16:37:24 -0300

    duda con js en agregar espectaculo a paquete

---
+++ client/js/agregarEspectaculoaPaquete.js
@@ -0,0 +1,58 @@
+var obj_csv = {
+    size:0,
+    dataFile:[]
+};
+ 
+document.querySelector('#plataforma').value="";
+
+function readImage(input) {
+    console.log(input)
+ if (input.files && input.files[0]) {
+	 let reader = new FileReader();
+	        reader.readAsText(input.files[0]);
+	 reader.onload = function (e) {
+		 console.log(e);
+		 obj_csv.size = e.total;
+		 obj_csv.dataFile = e.target.result
+		 console.log(obj_csv.dataFile)
+		 parseData(obj_csv.dataFile)		            
+	}
+ }
+
+ document.querySelector('#file-input').toggleAttribute('hidden');
+}
+
+var espectaculos = [];
+ 
+function parseData(data){
+    let csvData = [];
+    let lbreak = data.split("nl");
+    lbreak.forEach(res => {
+        csvData.push(res.split(";"));
+    });
+    cargarDatos(csvData);
+}
+
+var espectaculosLista = document.querySelector("#espectaculos-lista");
+function cargarDatos(espectaculos) {
+    var mytable = '<div class="card-deck pt-3">';
+    console.log(espectaculos);
+    for( var i=0; i<espectaculos.length; i++) {
+        titulo = espectaculos[i][2];
+        desc = espectaculos[i][3];
+        if ((i+1)%3 == 1 && i !=0) {
+            mytable += '</div><div class="card-deck pt-3">';
+        }
+        mytable += `<carta-espectaculo class="carta" img='${espectaculos[i][10]}' titulo='${espectaculos[i][2]}' artista='${espectaculos[i][0]}' descripcion="${espectaculos[i][3]}" precio="${espectaculos[i][8]}"></carta-espectaculo>`;
+    }
+    if  (!((i+1)%3 == 1)) {
+        mytable+='</div>'
+    }
+    espectaculosLista.innerHTML = mytable;
+};
+function mostrarDatosPaquete(value){
+   (value == "Bandas") ? cargarDatos([espectaculo3,espectaculo5,espectaculo6]) : espectaculosLista.innerHTML = "";
+   (value == "Solistas") ? cargarDatos([espectaculo3,espectaculo5,espectaculo6]) : espectaculosLista.innerHTML = "";
+   (value == "Latino") ? cargarDatos([espectaculo3,espectaculo5,espectaculo6]) : espectaculosLista.innerHTML = "";
+	
+}
