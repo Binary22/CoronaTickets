4ac93570fb04695bcc9bcd5b0c4d2f63ee0688c1
Santiago <santiacquarone@gmail.com>
2020-10-07 13:02:33 -0300

    Añadir estructura web server

---
+++ webserver/WebContent/WEB-INF/home/home.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+<%@page errorPage="/WEB-INF/500.jsp" %>
+<!doctype html>
+<html>
+   <head>
+	   <jsp:include page="/WEB-INF/template/head.jsp"/>
+    </head>
+    <body>
+        <jsp:include page="/WEB-INF/template/navbar.jsp"/>
+
+
+	</body>
+</html>
---
+++ webserver/src/controladores/Home.java
@@ -0,0 +1,44 @@
+package controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Home() {
+    	super();
+    }
+     
+	private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/home/iniciar.jsp").forward(req, resp);
+	}
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+}
