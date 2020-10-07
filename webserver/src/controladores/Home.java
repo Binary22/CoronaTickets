d025cc06df36fac7cf21af99204050908b950e2f
Santiago <santiacquarone@gmail.com>
2020-10-07 17:08:38 -0300

    Arreglar un cacho tema servlets

--- webserver/src/controladores/Home.java
+++ webserver/src/controladores/Home.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Home
  */
-@WebServlet("/home")
+
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
