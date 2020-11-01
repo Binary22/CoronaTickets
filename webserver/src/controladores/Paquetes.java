c44c93fd65b738adfea1ffbe289056ebb6977133
Joaquin <jotaseis@gmail.com>
2020-11-01 01:55:20 -0300

    listar paquetes.

--- webserver/src/controladores/Paquetes.java
+++ webserver/src/controladores/Paquetes.java
@@ -1,11 +1,26 @@
 package controladores;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import excepciones.NoExistePaqueteException;
+import logica.Fabrica;
+import logica.HandlerPaquetes;
+import logica.HandlerUsuarios;
+import logica.IConsulta;
+import logica.IEspectaculo;
+import logica.IPaquete;
+import logica.Paquete;
+import logica.Usuario;
 
 /**
  * Servlet implementation class Paquetes
@@ -22,6 +37,25 @@ public class Paquetes extends HttpServlet {
     }
 
     private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession objSesion = req.getSession();
+		HandlerPaquetes hp = HandlerPaquetes.getInstance();
+		List<String> paquetes = hp.getNombresPaquete();
+		
+		Map<String, Paquete> mapPaq = new HashMap<String, Paquete>();
+		
+		for(String p : paquetes ) {
+			try {
+			Paquete paq = hp.getPaquete(p);
+			mapPaq.put(p, paq);
+			} catch (NoExistePaqueteException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		objSesion.setAttribute("paquetes", mapPaq);
+		
 		req.getRequestDispatcher("/WEB-INF/paquetes/paquetes.jsp").forward(req, resp);
 	}
     
