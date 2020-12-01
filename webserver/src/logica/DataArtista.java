c306a3588b5fda48b7002476e14f76970d561573
Santiago <santiacquarone@gmail.com>
2020-11-30 21:49:53 -0300

    Merge favoritos con master

--- webserver/src/logica/DataArtista.java
+++ webserver/src/logica/DataArtista.java
@@ -41,9 +41,9 @@ public class DataArtista
 {
 
     @XmlElementRefs({
+        @XmlElementRef(name = "biografia", type = JAXBElement.class, required = false),
         @XmlElementRef(name = "website", type = JAXBElement.class, required = false),
-        @XmlElementRef(name = "descripcion", type = JAXBElement.class, required = false),
-        @XmlElementRef(name = "biografia", type = JAXBElement.class, required = false)
+        @XmlElementRef(name = "descripcion", type = JAXBElement.class, required = false)
     })
     protected List<JAXBElement<String>> rest;
 
@@ -53,8 +53,8 @@ public class DataArtista
      * <p>
      * You are getting this "catch-all" property because of the following reason: 
      * The field name "Descripcion" is used by two different parts of a schema. See: 
-     * line 317 of http://localhost:9129/publicador?xsd=1
-     * line 104 of http://localhost:9129/publicador?xsd=1
+     * line 319 of http://localhost:9129/publicador?xsd=1
+     * line 98 of http://localhost:9129/publicador?xsd=1
      * <p>
      * To get rid of this property, apply a property customization to one 
      * of both of the following declarations to change their names: 
